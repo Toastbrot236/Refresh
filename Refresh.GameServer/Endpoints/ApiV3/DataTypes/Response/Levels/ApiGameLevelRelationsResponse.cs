bbff63df97cadab2bd342a65337ab9edf682d156
Toaster <brottoastlemon@gmail.com>
2025-05-16 22:27:18 +0200

    extend ApiGameLevelRelationsResponse

--- Refresh.GameServer/Endpoints/ApiV3/DataTypes/Response/Levels/ApiGameLevelRelationsResponse.cs
+++ Refresh.GameServer/Endpoints/ApiV3/DataTypes/Response/Levels/ApiGameLevelRelationsResponse.cs
@@ -1,3 +1,7 @@
+using Refresh.Database.Models.Levels;
+using Refresh.Database.Models.Users;
+using Refresh.GameServer.Types.Data;
+
 namespace Refresh.GameServer.Endpoints.ApiV3.DataTypes.Response.Levels;
 
 [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
@@ -5,5 +9,20 @@ public class ApiGameLevelRelationsResponse : IApiResponse
 {
     public required bool IsHearted { get; set; }
     public required bool IsQueued { get; set; }
-    public required int MyPlaysCount { get; set; }
+    public required int TotalPlays { get; set; }
+    public required int PhotosTaken { get; set; }
+
+    public static ApiGameLevelRelationsResponse? FromOld(GameLevel level, DataContext dataContext)
+    {
+        GameUser? user = dataContext.User;
+        if (user == null) return null;
+
+        return new ApiGameLevelRelationsResponse
+        {
+            IsHearted = dataContext.Database.IsLevelFavouritedByUser(level, user),
+            IsQueued = dataContext.Database.IsLevelQueuedByUser(level, user),
+            TotalPlays = dataContext.Database.GetTotalPlaysForLevelByUser(level, user),
+            PhotosTaken = dataContext.Database.GetTotalPhotosInLevelByUser(level, user),
+        };
+    }
 }
