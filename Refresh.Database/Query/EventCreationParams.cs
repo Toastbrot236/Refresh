cfc45eb0cb3d70a267c569c73a606f36fc495f64
Toaster2 <brottoastlemon@gmail.com>
2025-10-22 13:29:09 +0200

    Define EventCreationParams to be a struct

--- Refresh.Database/Query/EventCreationParams.cs
+++ Refresh.Database/Query/EventCreationParams.cs
@@ -3,11 +3,14 @@ using Refresh.Database.Models.Users;
 
 namespace Refresh.Database.Query;
 
-public class EventCreationParams
+public struct EventCreationParams
 {
     public required EventType EventType { get; set; }
     public required GameUser Actor { get; set; }
     public required EventOverType OverType { get; set; }
     public bool IsModified { get; set; } = false;
     public string AdditionalInfo { get; set; } = "";
+
+    public EventCreationParams()
+    {}
 }
