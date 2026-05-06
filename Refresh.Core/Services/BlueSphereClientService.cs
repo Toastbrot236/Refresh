392f141c50e5fdde1ca44a1368b1809fabc0c30f
Toaster2 <brottoastlemon@gmail.com>
2026-05-06 13:44:01 +0200

    Remove unused attributes, slightly improve logging

--- Refresh.Core/Services/BlueSphereClientService.cs
+++ Refresh.Core/Services/BlueSphereClientService.cs
@@ -15,22 +15,13 @@ namespace Refresh.Core.Services;
 public class BlueSphereClientService : EndpointService
 {
     private readonly HttpClient _client;
-    private readonly GameServerConfig _config;
-    private readonly IntegrationConfig _integration;
-    private readonly ContactInfoConfig _contact;
-    private readonly TimeProviderService _timeProviderService;
 
-    public BlueSphereClientService(Logger logger, GameServerConfig config, IntegrationConfig integration, ContactInfoConfig contact, TimeProviderService timeProviderService) : base(logger)
+    public BlueSphereClientService(Logger logger, GameServerConfig config, IntegrationConfig integration, ContactInfoConfig contact) : base(logger)
     {
         if (!integration.BlueSphereEnabled)
         {
             throw new InvalidOperationException($"Cannot construct BlueSphereClientService because integration is disabled. This should be caught beforehand!");
         }
-
-        this._config = config;
-        this._integration = integration;
-        this._contact = contact;
-        this._timeProviderService = timeProviderService;
 
         this._client = new HttpClient
         {
@@ -52,6 +43,7 @@ public class BlueSphereClientService : EndpointService
 
         string serializedRequest = JsonConvert.SerializeObject(request);
         this.Logger.LogDebug(BunkumCategory.Authentication, $"Sending verification request to BlueSphere for username '{username}', code '{code}': '{serializedRequest}'");
+
         HttpResponseMessage message = await this._client.PostAsync("/api/verify", new StringContent(serializedRequest));
 
         string serializedResponse = Encoding.UTF8.GetString(await message.Content.ReadAsByteArrayAsync());
@@ -63,11 +55,11 @@ public class BlueSphereClientService : EndpointService
             BsErrorResponse? error = JsonConvert.DeserializeObject<BsErrorResponse>(serializedResponse);
             if (error == null) throw new BsOAuthException($"Couldn't deserialize error response for {code}! (status: {message.StatusCode})");
 
-            throw new BsOAuthException($"Authentication failed for {code} (status: {message.StatusCode}): '{error.Error}'");
+            throw new BsOAuthException($"Authentication failed for code '{code}' (status: {message.StatusCode}): '{error.Error}'");
         }
 
         BsOAuthResponse? success = JsonConvert.DeserializeObject<BsOAuthResponse>(serializedResponse);
-        if (success == null) throw new BsOAuthException($"Couldn't deserialize success response for {code}!");
+        if (success == null) throw new BsOAuthException($"Couldn't deserialize success response for code '{code}'!");
 
         return success;
     }
