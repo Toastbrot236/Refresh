d0848d7530f38da791018528cd396ba227419917
Toaster <brottoastlemon@gmail.com>
2026-09-21 11:31:33 +0200

    Add API buckets to enum and defaults

--- Refresh.Core/RateLimits/EndpointRateLimiting/EndpointBucketDefaults.cs
+++ Refresh.Core/RateLimits/EndpointRateLimiting/EndpointBucketDefaults.cs
@@ -10,11 +10,131 @@ public static class EndpointBucketDefaults
         #region Misc
         {EndpointBucketId.Default, new(90, 300, 45)},
         #endregion
+
+        #region Authentication
+        {EndpointBucketId.ApiLogin, new(300, 10, 300)},
+        {EndpointBucketId.ApiRegister, new(3600, 10, 1800)},
+
+        {EndpointBucketId.ApiRequestEmail, new(300, 10, 300)},
+        {EndpointBucketId.ApiVerifyEmailAddress, new(300, 10, 300)},
+        {EndpointBucketId.ApiResetPassword, new(300, 10, 300)},
+
+        {EndpointBucketId.ApiGetListOfIpAddresses, new(300, 30, 240)},
+        {EndpointBucketId.ApiApproveOrDenyIpAddress, new(300, 30, 240)},
+
+        {EndpointBucketId.ApiDeleteOwnUser, new(600, 6, 480)},
+        #endregion
+
+        #region Categories
+        {EndpointBucketId.ApiGetListOfCategories, new(240, 20, 180)},
+        #endregion
+
+        #region Instance
+        {EndpointBucketId.ApiGetInstanceInfo, new(240, 30, 180)},
+        {EndpointBucketId.ApiGetInstanceStats, new(240, 30, 180)},
+        {EndpointBucketId.ApiGetDocumentation, new(240, 30, 180)},
+        {EndpointBucketId.ApiGetListOfAnnouncements, new(240, 30, 180)},
+        #endregion
         
         #region Levels
-        // game sometimes requests many levels in bursts
         {EndpointBucketId.GameGetSingleLevel, new(240, 200, 180)},
+
+        {EndpointBucketId.ApiGetListOfLevels, new(240, 50, 180)},
         {EndpointBucketId.ApiGetSingleLevel, new(240, 50, 180)},
+        {EndpointBucketId.ApiGetOwnRelationsToLevel, new(240, 50, 180)},
+
+        {EndpointBucketId.ApiEditLevel, new(300, 20, 180)},
+
+        {EndpointBucketId.DeleteLevel, new(300, 20, 180)},
+        {EndpointBucketId.HeartLevel, new(300, 30, 180)},
+        {EndpointBucketId.QueueLevel, new(300, 50, 180)},
+        {EndpointBucketId.TagLevel, new(300, 10, 180)},
+        {EndpointBucketId.RateLevel, new(300, 20, 180)},
+        #endregion
+
+        #region Level Scores
+        {EndpointBucketId.ApiGetListOfLevelScores, new(300, 40, 180)},
+        {EndpointBucketId.ApiGetSingleLevelScore, new(300, 40, 180)},
+        #endregion
+
+        #region Reviews
+        {EndpointBucketId.ApiGetListOfReviews, new(300, 40, 180)},
+        {EndpointBucketId.ApiGetSingleReview, new(300, 40, 180)},
+
+        {EndpointBucketId.UploadReview, new(300, 12, 180)},
+        {EndpointBucketId.RateReview, new(300, 40, 180)},
+        {EndpointBucketId.DeleteReview, new(300, 30, 180)},
+        #endregion
+
+        #region Comments (both Profile and Level)
+        {EndpointBucketId.ApiGetListOfComments, new(300, 40, 180)},
+        {EndpointBucketId.ApiGetSingleComment, new(300, 40, 180)},
+
+        {EndpointBucketId.UploadComment, new(300, 18, 180)},
+        {EndpointBucketId.RateComment, new(300, 40, 180)},
+        {EndpointBucketId.DeleteComment, new(300, 30, 180)},
+        #endregion
+
+        #region Photos
+        {EndpointBucketId.ApiGetListOfPhotos, new(300, 40, 180)},
+        {EndpointBucketId.ApiGetSinglePhoto, new(300, 40, 180)},
+
+        {EndpointBucketId.DeletePhoto, new(300, 30, 180)},
+        #endregion
+
+        #region Users
+        {EndpointBucketId.ApiGetListOfUsers, new(300, 60, 180)},
+        {EndpointBucketId.ApiGetSingleUser, new(300, 60, 180)},
+        // this should stay relatively high because currently, both websites will call this to find out whether the user
+        // is still authed every time they're refreshed, but also every time the user visits a new page
+        {EndpointBucketId.ApiGetOwnUser, new(300, 70, 180)},
+
+        {EndpointBucketId.UpdateUser, new(300, 20, 180)},
+        {EndpointBucketId.HeartUser, new(300, 30, 180)},
+
+        #endregion
+
+        #region Assets
+        // Regular download limits are this high on both game and API because both the game and third party API clients
+        // (e.g. archive_dl) are likely to download many of these at times depending on what level they're trying to load
+        // (additionally, adventures can have even more dependencies!)
+        {EndpointBucketId.ApiUploadImage, new(300, 20, 180)},
+        {EndpointBucketId.ApiDownloadAsset, new(240, 500, 120)},
+        {EndpointBucketId.ApiDownloadImage, new(240, 250, 120)},
+
+        {EndpointBucketId.ApiGetAssetMetadata, new(240, 250, 120)},
+        #endregion
+
+        #region Matching
+        // this high because of beta website's fake live updating (sends new request every few seconds), which we have to deal with for now
+        {EndpointBucketId.ApiGetListOfRooms, new(240, 90, 120)},
+        {EndpointBucketId.ApiGetSingleRoom, new(240, 40, 120)},
+        #endregion
+
+        #region Playlists
+        {EndpointBucketId.ApiGetListOfPlaylists, new(240, 50, 180)},
+        {EndpointBucketId.ApiGetSinglePlaylist, new(240, 50, 180)},
+
+        {EndpointBucketId.CreatePlaylist, new(240, 30, 180)},
+        {EndpointBucketId.UpdatePlaylistMetadata, new(240, 30, 180)},
+        {EndpointBucketId.UpdatePlaylistContents, new(240, 50, 180)},
+        {EndpointBucketId.HeartPlaylist, new(240, 30, 180)},
+        {EndpointBucketId.DeletePlaylist, new(240, 30, 180)},
+        #endregion
+
+        #region Activity
+        {EndpointBucketId.ApiGetActivityPage, new(240, 50, 180)},
+        #endregion
+
+        #region Notifications
+        {EndpointBucketId.ApiGetListOfNotifications, new(240, 20, 180)},
+        {EndpointBucketId.ApiGetSingleNotification, new(240, 20, 180)},
+        {EndpointBucketId.ApiDeleteNotification, new(240, 20, 180)},
+        #endregion
+
+        #region Contests
+        {EndpointBucketId.ApiGetListOfContests, new(240, 20, 180)},
+        {EndpointBucketId.ApiGetSingleContest, new(240, 20, 180)},
         #endregion
     }.ToFrozenDictionary();
 }
--- Refresh.Core/RateLimits/EndpointRateLimiting/EndpointBucketId.cs
+++ Refresh.Core/RateLimits/EndpointRateLimiting/EndpointBucketId.cs
@@ -1,16 +1,125 @@
 namespace Refresh.Core.RateLimits.EndpointRateLimiting;
 
-// TODO add IDs for all API/game buckets here
-// Generally, fetch endpoints should use separate buckets depending on whether they are game/API endpoints,
-// while upload/modification/deletion endpoints should share buckets.
 public enum EndpointBucketId
 {
     #region Misc
     Default,
     #endregion
+
+    #region Authentication
+    ApiLogin,
+    ApiRegister,
+    ApiRequestEmail,
+    ApiVerifyEmailAddress,
+    ApiResetPassword,
+    ApiGetListOfIpAddresses,
+    ApiApproveOrDenyIpAddress,
+    ApiDeleteOwnUser,
+    #endregion
+
+    #region Instance
+    ApiGetInstanceStats,
+    ApiGetInstanceInfo,
+    ApiGetDocumentation,
+
+    ApiGetListOfAnnouncements,
+    #endregion
+
+    #region Categories
+    ApiGetListOfCategories,
+    #endregion
     
     #region Levels
     GameGetSingleLevel,
     ApiGetSingleLevel,
+    ApiGetOwnRelationsToLevel,
+    
+    ApiGetListOfLevels,
+
+    ApiEditLevel,
+
+    DeleteLevel,
+    HeartLevel,
+    QueueLevel,
+    TagLevel,
+    RateLevel,
+    ApiOverrideLevel,
+    #endregion
+
+    #region Level Scores
+    ApiGetListOfLevelScores,
+    ApiGetSingleLevelScore,
+    #endregion
+
+    #region Reviews
+    ApiGetListOfReviews,
+    ApiGetSingleReview,
+
+    UploadReview,
+    RateReview,
+    DeleteReview,
+    #endregion
+
+    #region Comments (both Profile and Level)
+    ApiGetListOfComments,
+    ApiGetSingleComment,
+
+    UploadComment,
+    RateComment,
+    DeleteComment,
+    #endregion
+
+    #region Photos
+    ApiGetListOfPhotos,
+    ApiGetSinglePhoto,
+
+    DeletePhoto,
+    #endregion
+
+    #region Users
+    ApiGetListOfUsers,
+    ApiGetSingleUser,
+    ApiGetOwnUser,
+
+    UpdateUser,
+    HeartUser,
+    #endregion
+
+    #region Assets
+    ApiDownloadAsset,
+    ApiDownloadImage,
+    ApiGetAssetMetadata,
+    ApiUploadImage,
+    #endregion
+
+    #region Matching
+    ApiGetListOfRooms,
+    ApiGetSingleRoom,
+    #endregion
+
+    #region Playlists
+    ApiGetListOfPlaylists,
+    ApiGetSinglePlaylist,
+
+    CreatePlaylist,
+    UpdatePlaylistMetadata,
+    UpdatePlaylistContents,
+    HeartPlaylist,
+    DeletePlaylist,
+    #endregion
+
+    #region Activity
+    ApiGetActivityPage,
+    #endregion
+
+    #region Notifications
+    ApiGetListOfNotifications,
+    ApiGetSingleNotification,
+    ApiDeleteNotification,
+    #endregion
+
+    #region Contests
+    ApiGetListOfContests,
+    ApiGetSingleContest,
     #endregion
 }
