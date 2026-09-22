2d8ddc30942dd29968262f114eee0499e1af2416
Toaster <brottoastlemon@gmail.com>
2026-09-22 22:23:32 +0200

    Don't look up levels by username when searching levels by query

--- Refresh.Database/GameDatabaseContext.Levels.cs
+++ Refresh.Database/GameDatabaseContext.Levels.cs
@@ -504,13 +504,6 @@ public partial class GameDatabaseContext // Levels
                 levels.Add(idLevel);
             }
         }
-        
-        // Try to look up a username to search by publisher.
-        GameUser? publisher = this.GetUserByUsername(query, false); 
-        if (publisher != null)
-        {
-            levels.AddRange(validLevels.Where(l => l.Publisher == publisher));
-        }
 
         return new DatabaseList<GameLevel>(levels.OrderByDescending(l => l.CoolRating), skip, count);
     }
