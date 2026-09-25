10f3b25c0579b4be26605558e1ee590fb43b34c0
Toaster <brottoastlemon@gmail.com>
2026-09-26 00:17:03 +0200

    Remove no longer necessary param overwrites in API endpoints

--- Refresh.Interfaces.APIv3/Endpoints/CategoryApiEndpoints.cs
+++ Refresh.Interfaces.APIv3/Endpoints/CategoryApiEndpoints.cs
@@ -71,12 +71,6 @@ public class CategoryApiEndpoints : EndpointGroup
         
         (int skip, int count) = context.GetPageData();
         
-        LevelFilterSettings filters = LevelFilterSettings.FromApiRequest(context);
-        // We only care about levels here, so avoid user and playlist lookups if this category supports them.
-        filters.DisplayLevels = true;
-        filters.DisplayUsers = false;
-        filters.DisplayPlaylists = false;
-
         DatabaseList<GameLevel>? list = categories.LevelCategories
             .FirstOrDefault(c => c.ApiRoute.StartsWith(route))?
             .Fetch(context, skip, count, dataContext, LevelFilterSettings.FromApiRequest(context), user)?
@@ -137,16 +131,10 @@ public class CategoryApiEndpoints : EndpointGroup
 
         if (!config.PermitShowingOnlineUsers) return ApiNotFoundError.Instance;
         (int skip, int count) = context.GetPageData();
-
-        LevelFilterSettings filters = LevelFilterSettings.FromApiRequest(context);
-        // We only care about users here, so avoid level and playlist lookups if this category supports them.
-        filters.DisplayLevels = false;
-        filters.DisplayUsers = true;
-        filters.DisplayPlaylists = false;
         
         DatabaseList<GameUser>? list = categories.UserCategories
             .FirstOrDefault(c => c.ApiRoute.StartsWith(route))?
-            .Fetch(context, skip, count, dataContext, filters, user)?
+            .Fetch(context, skip, count, dataContext, LevelFilterSettings.FromApiRequest(context), user)?
             .Users;
 
         if (list == null) return ApiNotFoundError.Instance;
