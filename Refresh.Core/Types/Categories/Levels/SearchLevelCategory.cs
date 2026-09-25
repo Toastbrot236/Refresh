c7f0c183d0090bd1d29f2cd814553f3f0540c5e0
Toaster <brottoastlemon@gmail.com>
2026-09-25 17:16:04 +0200

    Mixed search category for the games

--- Refresh.Core/Types/Categories/Levels/SearchLevelCategory.cs
+++ Refresh.Core/Types/Categories/Levels/SearchLevelCategory.cs
@@ -1,16 +1,27 @@
 using Bunkum.Core;
 using Refresh.Core.Types.Data;
+using Refresh.Database;
+using Refresh.Database.Models.Authentication;
+using Refresh.Database.Models.Levels;
 using Refresh.Database.Models.Users;
 using Refresh.Database.Query;
 
 namespace Refresh.Core.Types.Categories.Levels;
 
+// Since the games only want one search category to return all entity types, we will have to do so for them.
+// Additionally, LBP1/2 use /slots/search for searching, so it has to be a level category.
+// For the API however, we should keep searching for different entity types in separate categories for now.
+// This is due to current limitations in APIv3 spec (level categories may only return levels, user categories only users etc.)
+// I couldn't find a way to have one search category for API without having something very hacky or messy in general,
+// and separate categories should be enough for now. I really don't feel like thinking about this anymore,
+// so if we really do want one for all types eventually, we should think of it when that time comes.
+// Doing it like this (for now) will keep us from adding new stuff to the spec we might regret later on anyway.
 public class SearchLevelCategory : GameCategory
 {
     internal SearchLevelCategory() : base("search", "search", false)
     {
         this.Name = "Search";
-        this.Description = "Search for new levels.";
+        this.Description = "Search for levels by name and description.";
         this.FontAwesomeIcon = "magnifying-glass";
         // no icon for now, too lazy to find
         this.Hidden = true; // The search category is not meant to be shown, as it requires a special implementation on all frontends
@@ -25,6 +36,33 @@ public class SearchLevelCategory : GameCategory
                         ?? context.QueryString["textFilter"]; // LBP3 sends this instead of query
         if (query == null) return null;
 
-        return new(dataContext.Database.SearchForLevels(count, skip, dataContext.User, levelFilterSettings, query));
+        // Unlike the dedicated search categories for the other
+        DatabaseList<GameLevel>? levels = !levelFilterSettings.DisplayLevels
+            ? null
+            : dataContext.Database.SearchForLevels(count, skip, dataContext.User, levelFilterSettings, query);
+        
+        // The API endpoint methods already override entity type params themselves to avoid DB calls for types
+        // they don't want, so we don't have to do anything special here.
+        // TODO Allow specifying custom params in queries, so users could filter entity types in LBP1/2 as well.
+        // TODO Also allow users to explicitly tell us to not search in name or description.
+        //
+        // Additionally, for some reason, LBP1 shows user results as large instead of small polaroids.
+        // We should return less users than requested there to not make the polaroids/badges too messy.
+        // We also need to modify skip for this so we don't skip over users when paginating.
+        int userCount = count;
+        int userSkip = skip;
+        if (levelFilterSettings.GameVersion == TokenGame.LittleBigPlanet1)
+        {
+            userCount /= 3;
+            userSkip /= 3;
+        }
+        
+        DatabaseList<GameUser>? users = !levelFilterSettings.DisplayUsers
+            ? null
+            : dataContext.Database.SearchForUsers(userCount, userSkip, query);
+        
+        // TODO also allow searching and returning playlists
+        
+        return new(levels, users, null);
     }
 }
