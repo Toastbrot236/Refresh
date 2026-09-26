f63ab0a66792a694a856e5ec6ed6e975ce817198
Toaster <brottoastlemon@gmail.com>
2026-09-26 16:21:28 +0200

    Add Admin category tests

---
+++ RefreshTests.GameServer/Tests/Moderation/AdminCategoryTests.cs
@@ -0,0 +1,180 @@
+using Refresh.Database.Models.Authentication;
+using Refresh.Database.Models.Users;
+using Refresh.Interfaces.APIv3.Endpoints.ApiTypes;
+using Refresh.Interfaces.APIv3.Endpoints.DataTypes.Response.Categories;
+using Refresh.Interfaces.APIv3.Endpoints.DataTypes.Response.Users;
+using Refresh.Interfaces.Game.Types.Lists;
+using RefreshTests.GameServer.Extensions;
+
+namespace RefreshTests.GameServer.Tests.Moderation;
+
+public class AdminCategoryTests : GameServerTest
+{
+    private void PrepareUsers(TestContext context)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            context.CreateUser();
+        }
+    }
+    
+    [Test]
+    public void AdminCategoriesInaccessibleViaApiIfUnauthed()
+    {
+        using TestContext context = this.GetServer();
+        this.PrepareUsers(context);
+
+        // Can't discover them
+        HttpResponseMessage message = context.Http.GetAsync("/api/v3/admin/userCategories").Result;
+        Assert.That(message.StatusCode, Is.EqualTo(Forbidden));
+
+        // Can't use newest user redirect
+        message = context.Http.GetAsync("/api/v3/admin/users").Result;
+        Assert.That(message.StatusCode, Is.EqualTo(Forbidden));
+
+        // Can't use the 2 existing categories
+        message = context.Http.GetAsync("/api/v3/admin/userCategories/newest").Result;
+        Assert.That(message.StatusCode, Is.EqualTo(Forbidden));
+        message = context.Http.GetAsync("/api/v3/admin/userCategories/searchAddress?query=local").Result;
+        Assert.That(message.StatusCode, Is.EqualTo(Forbidden));
+        
+        // Can't use non-existent category
+        message = context.Http.GetAsync("/api/v3/admin/userCategories/grinchy").Result;
+        Assert.That(message.StatusCode, Is.EqualTo(Forbidden));
+    }
+    
+    [Test]
+    [TestCase(GameUserRole.User)]
+    [TestCase(GameUserRole.Restricted)]
+    public void AdminCategoriesInaccessibleToNonStaff(GameUserRole role)
+    {
+        using TestContext context = this.GetServer();
+        this.PrepareUsers(context);
+
+        // Can't discover them
+        HttpResponseMessage message = context.Http.GetAsync("/api/v3/admin/userCategories").Result;
+        Assert.That(message.StatusCode, Is.EqualTo(Forbidden));
+
+        // Can't use newest user redirect
+        message = context.Http.GetAsync("/api/v3/admin/users").Result;
+        Assert.That(message.StatusCode, Is.EqualTo(Forbidden));
+
+        // Can't use the 2 existing categories
+        message = context.Http.GetAsync("/api/v3/admin/userCategories/newest").Result;
+        Assert.That(message.StatusCode, Is.EqualTo(Forbidden));
+        message = context.Http.GetAsync("/api/v3/admin/userCategories/searchAddress?query=local").Result;
+        Assert.That(message.StatusCode, Is.EqualTo(Forbidden));
+        
+        // Can't use non-existent category
+        message = context.Http.GetAsync("/api/v3/admin/userCategories/grinchy").Result;
+        Assert.That(message.StatusCode, Is.EqualTo(Forbidden));
+    }
+    
+    [Test]
+    [TestCase(GameUserRole.Admin)]
+    [TestCase(GameUserRole.Moderator)]
+    public void AdminCategoriesAccessibleToStaff(GameUserRole role)
+    {
+        using TestContext context = this.GetServer();
+        this.PrepareUsers(context);
+        GameUser accessor = context.CreateUser(role: role);
+        using HttpClient client = context.GetAuthenticatedClient(TokenType.Api, accessor);
+
+        // Can discover them
+        ApiListResponse<ApiCategoryResponse>? categories = client.GetList<ApiCategoryResponse>("/api/v3/admin/userCategories");
+        Assert.That(categories?.Data, Is.Not.Null);
+        Assert.That(categories!.Data!.Any(c => c.ApiRoute == "searchAddress"), Is.True);
+
+        // Can use newest user redirect
+        ApiListResponse<ApiExtendedGameUserResponse>? list = client.GetList<ApiExtendedGameUserResponse>("/api/v3/admin/users");
+        Assert.That(list?.Data, Is.Not.Null);
+        Assert.That(list!.ListInfo, Is.Not.Null);
+        Assert.That(list!.ListInfo!.TotalItems, Is.EqualTo(11)); // our 10 users + the mod
+        Assert.That(list!.ListInfo!.TotalItems, Is.EqualTo(list!.Data!.Count));
+
+        // Can use the extended newest API category
+        list = client.GetList<ApiExtendedGameUserResponse>("/api/v3/admin/userCategories/newest");
+        Assert.That(list?.Data, Is.Not.Null);
+        Assert.That(list!.ListInfo, Is.Not.Null);
+        Assert.That(list!.ListInfo!.TotalItems, Is.EqualTo(11)); // our 10 users + the mod
+        Assert.That(list!.ListInfo!.TotalItems, Is.EqualTo(list!.Data!.Count));
+        
+        // Can search users (query correctly affects results)
+        list = client.GetList<ApiExtendedGameUserResponse>("/api/v3/admin/userCategories/searchAddress?query=101");
+        Assert.That(list?.Data, Is.Not.Null);
+        Assert.That(list!.ListInfo, Is.Not.Null);
+        Assert.That(list!.ListInfo!.TotalItems, Is.EqualTo(1)); // the one user who has 101 in their email
+        Assert.That(list!.ListInfo!.TotalItems, Is.EqualTo(list!.Data!.Count));
+        Assert.That(list!.Data.First().EmailAddress, Is.EqualTo("101@101.local"));
+        
+        list = client.GetList<ApiExtendedGameUserResponse>("/api/v3/admin/userCategories/searchAddress?query=local");
+        Assert.That(list?.Data, Is.Not.Null);
+        Assert.That(list!.ListInfo, Is.Not.Null);
+        Assert.That(list!.ListInfo!.TotalItems, Is.EqualTo(11)); // the one user who has 101 in their email
+        Assert.That(list!.ListInfo!.TotalItems, Is.EqualTo(list!.Data!.Count));
+        Assert.That(list!.Data.All(u => u.EmailAddress != null && u.EmailAddress.Contains("local")), Is.True);
+        
+        // Missing query should fail
+        list = client.GetList<ApiExtendedGameUserResponse>("/api/v3/admin/userCategories/searchAddress", false, true);
+        Assert.That(list?.Error, Is.Not.Null);
+        Assert.That(list?.Data, Is.Null);
+        
+        // Can't use non-existent category
+        list = client.GetList<ApiExtendedGameUserResponse>("/api/v3/admin/userCategories/grinchy", false, true);
+        Assert.That(list?.Error, Is.Not.Null);
+        Assert.That(list!.Error!.Name, Is.EqualTo("ApiNotFoundError"));
+    }
+    
+    [Test]
+    [TestCase(GameUserRole.Admin)]
+    [TestCase(GameUserRole.Moderator)]
+    [TestCase(GameUserRole.User)]
+    [TestCase(GameUserRole.Restricted)]
+    public void SearchByAddressInaccessibleIfLBP3Category(GameUserRole role)
+    {
+        using TestContext context = this.GetServer();
+        this.PrepareUsers(context);
+        GameUser accessor = context.CreateUser(role: role);
+        using HttpClient client = context.GetAuthenticatedClient(TokenType.Game, TokenGame.LittleBigPlanet3, TokenPlatform.PS3, accessor);
+        
+        // Ensure it's not discoverable
+        HttpResponseMessage message = client.GetAsync($"/lbp/searches?count=67").Result;
+        Assert.That(message.StatusCode, Is.EqualTo(OK));
+        
+        // Make sure neither the address category isn't in the list nor the dedicated search property
+        SerializedCategoryList result = message.Content.ReadAsXML<SerializedCategoryList>();
+        Assert.That(result.Items.Any(c => c.Url.Contains("searchAddress")), Is.False);
+        Assert.That(result.TextSearchCategory.Url, Does.Not.Contain("searchAddress"));
+        
+        // Also do this for the genre categories
+        message = client.GetAsync($"/lbp/genres?count=67").Result;
+        Assert.That(message.StatusCode, Is.EqualTo(OK));
+        
+        result = message.Content.ReadAsXML<SerializedCategoryList>();
+        Assert.That(result.Items.Any(c => c.Url.Contains("searchAddress")), Is.False);
+        Assert.That(result.TextSearchCategory.Url, Does.Not.Contain("searchAddress"));
+        
+        // Ensure it can't be requested separately
+        message = client.GetAsync($"/lbp/searches/users/searchAddress").Result;
+        Assert.That(message.StatusCode, Is.EqualTo(NotFound));
+        message = client.GetAsync($"/lbp/searches/levels/searchAddress").Result;
+        Assert.That(message.StatusCode, Is.EqualTo(NotFound));
+    }
+    
+    [Test]
+    [TestCase(GameUserRole.Admin)]
+    [TestCase(GameUserRole.Moderator)]
+    [TestCase(GameUserRole.User)]
+    [TestCase(GameUserRole.Restricted)]
+    public void SearchByAddressInaccessibleIfLBPSlotsEndpoint(GameUserRole role)
+    {
+        using TestContext context = this.GetServer();
+        this.PrepareUsers(context);
+        GameUser accessor = context.CreateUser(role: role);
+        using HttpClient client = context.GetAuthenticatedClient(TokenType.Game, TokenGame.LittleBigPlanet2, TokenPlatform.PS3, accessor);
+        
+        // Ensure it's also not somehow accessible via /slots/{route}, whyever that would happen
+        HttpResponseMessage message = client.GetAsync($"/lbp/slots/searchAddress?count=67").Result;
+        Assert.That(message.StatusCode, Is.EqualTo(NotFound));
+    }
+}
